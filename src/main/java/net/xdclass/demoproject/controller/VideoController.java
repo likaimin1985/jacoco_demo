73b16b97ee4ee154167a30c41ad1e4ac33bbdd97
likaimin1985 <likaimin1985@163.com>
2020-07-29 17:41:38 -0400

    101_first

---
+++ src/main/java/net/xdclass/demoproject/controller/VideoController.java
@@ -0,0 +1,43 @@
+package net.xdclass.demoproject.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 视频控制器
+ */
+
+
+//@Controller
+@RestController
+@RequestMapping("/api")
+//@SpringBootApplication
+public class VideoController {
+
+
+
+    @RequestMapping("/hello")
+    public String list(String num){
+        try {
+            Integer n = Integer.parseInt(num);
+            if (n > 0) {
+                return "整数";
+            } else if (n == 0) {
+                return "零";
+            } else {
+                return "负数";
+            }
+        }catch (Exception e){
+            return "不是数字";
+        }
+
+    }
+
+
+
+}
